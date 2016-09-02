87de1c4fae992713ddd442d059f3ba100e1c16bc
Armando Magalhães <armando.mag95@gmail.com>
2016-09-02 01:26:30 -0300

    refactor(task-list): cleaner code

--- app/components/views/TaskList.jsx
+++ app/components/views/TaskList.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
 
 export default class TaskList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    return (
-      <ul>
-        { this.props.children }
-      </ul>
-    )
+    return (<ul>{ this.props.children }</ul>)
   }
 }
