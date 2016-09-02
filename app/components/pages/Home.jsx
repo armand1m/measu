acff934948d22a3ddde13d7fe182ed6bdce08331
Armando Magalhães <armando.mag95@gmail.com>
2016-09-02 01:23:47 -0300

    feat(home): Better layout

--- app/components/pages/Home.jsx
+++ app/components/pages/Home.jsx
@@ -3,15 +3,33 @@ import TaskListContainer from '../containers/TaskListContainer.jsx';
 import TaskFormContainer from '../containers/TaskFormContainer.jsx';
 import ProjectDetailsContainer from '../containers/ProjectDetailsContainer.jsx';
 
+const imageStyle = {
+  width: "5rem",
+  borderRadius: "50%",
+  margin: "1rem"
+}
+
+const titleStyle = {
+  alignItems: "center",
+  display: "flex",
+  justifyContent: "center"
+}
+
 export default class Home extends React.Component {
   render() {
     return (
       <div className="content">
-        <h1 className="title is-1">measu.</h1>
-        <p className="subtitle is-3">
-          a project measurement tool by <a href="http://fluxor.org" target="_blank">Fluxor</a>.
-        </p>
-        <hr />
+        <div className="content has-text-centered">
+          <div className="title is-1" style={ titleStyle }>
+            <img src="images/photo.gif" style={ imageStyle }/>
+            measu.
+          </div>
+          <p className="subtitle is-3">
+            a project measurement tool by <a href="http://fluxor.org" target="_blank">Fluxor</a>.
+          </p>
+        </div>
+
+        <hr className="is-marginless" />
 
         <div className="columns">
           <div className="column is-two-thirds">
