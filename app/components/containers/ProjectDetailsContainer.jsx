a01db8d895d117dd52d77274941cdf5aa3464e50
Armando Magalhães <armando.mag95@gmail.com>
2016-09-05 08:18:31 -0300

    feat(container-components): Bindings to firebase

--- app/components/containers/ProjectDetailsContainer.jsx
+++ app/components/containers/ProjectDetailsContainer.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import config from '../../config';
 import ProjectDetails from '../views/ProjectDetails.jsx';
-import { getTaskTotalHours, getTaskTotalValue } from '../../services/task-service';
+import { getTaskTotalHours } from '../../services/task-service';
+import { setProject, changeProject } from '../../actions/project-actions';
+import ProjectService from '../../services/project-service';
 
 class ProjectDetailsContainer extends React.Component {
   constructor(props) {
@@ -10,6 +12,19 @@ class ProjectDetailsContainer extends React.Component {
 
     this.getTotalHours = this.getTotalHours.bind(this)
     this.getTotalValue = this.getTotalValue.bind(this)
+    this.onFieldChange = this.onFieldChange.bind(this)
+  }
+
+  componentDidMount() {
+    var projectRef = ProjectService.getReference()
+
+    projectRef.once('value', snapshot => {
+      this.props.dispatch(setProject(snapshot.val()))
+    })
+
+    projectRef.on('child_changed', snapshot => {
+      this.props.dispatch(changeProject(snapshot.key, snapshot.val()))
+    })
   }
   
   getTotalHours() {
@@ -22,22 +37,31 @@ class ProjectDetailsContainer extends React.Component {
     return this.getTotalHours() * config.valuePerHour
   }
 
+  onFieldChange(key, e) {
+    var data = {
+      [key]: e.target.value
+    }
+
+    ProjectService.update(null, data)
+  }
+
   render() {
     return (
       <ProjectDetails 
         valueUnit={ config.valueUnit }
-        valuePerHour={ config.valuePerHour }
         getTotalHours={ this.getTotalHours }
         getTotalValue={ this.getTotalValue }
+        onFieldChange={ this.onFieldChange }
+        project={ this.props.project }
         tasks={ this.props.tasks } />
     )
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  let { tasks } = state
+  let { tasks, project } = state
 
-  return { tasks }
+  return { tasks, project }
 }
 
 export default connect(mapStateToProps)(ProjectDetailsContainer);
--- app/components/containers/TaskFormContainer.jsx
+++ app/components/containers/TaskFormContainer.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import TaskForm from '../views/TaskForm.jsx';
 import { createTask } from '../../services/task-service';
-import { addTaskSuccess } from '../../actions/task-actions';
 
 class TaskFormContainer extends React.Component {
   render() {
