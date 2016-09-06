7fb7c37aecdcb11d889d8947434202a414a5e835
Armando Magalhães <armando.mag95@gmail.com>
2016-09-06 08:12:27 -0300

    feat(download.js): add download.js

--- app/app.js
+++ app/app.js
@@ -1,7 +1,10 @@
 import bulma from 'bulma/bulma.sass';
+import download from './vendor/download';
 import React from 'react';
 import { Provider } from 'react-redux';
 import router from './router';
 import store from './store';
+
+window.download = download
 
 export default ( <Provider style={ bulma } store={ store }>{ router }</Provider> );
