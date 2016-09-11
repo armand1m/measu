76ce0ad713c9e6156e7fb31af6f2fbb4c0dc5061
Armando Magalhães <armando.mag95@gmail.com>
2016-09-11 05:11:17 -0300

    chore(layout): remove unused code

--- app/components/layouts/MainLayout.jsx
+++ app/components/layouts/MainLayout.jsx
@@ -3,24 +3,6 @@ import MainHeader from '../views/MainHeader.jsx';
 import MainFooter from "../views/MainFooter.jsx"
 
 export default class MainLayout extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      sidebar: {
-        pinned: false
-      }
-    }
-
-    this.toggleSidebar = this.toggleSidebar.bind(this)
-  }
-
-  toggleSidebar() {
-    this.setState({ 
-      sidebar: { pinned: !this.state.sidebar.pinned }
-    })
-  }
-
   render() {
     return (
       <div className="container is-fluid">
