073a6d31dd0a4651609fbc01768f6376b9eb8a8b
Armando Magalhães <armando.mag95@gmail.com>
2016-10-02 13:18:15 -0300

    chore(dev-env): Development test data
    Development test data for firebase-dev-server

--- firebase-dev-server.js
+++ firebase-dev-server.js
@@ -1,26 +1,10 @@
 'use strict'
 
+const data = require('./dev-server-data.json')
 const cli = require('cli')
 const debug = require('debug')
 const FirebaseServer = require('firebase-server')
-const initialState = {
-  "projects": {
-    "1": {
-      "description" : "Teste",
-      "name" : "Test 1",
-      "valuePerHour" : "40"
-    },
-    "2": {
-      "description" : "Teste",
-      "name" : "Test 2",
-      "valuePerHour" : "35"
-    }
-  },
-  "tasks" : {
-    
-  }
-}
 
 debug.enable('firebase-server*')
 
-new FirebaseServer(3000, '127.0.0.1', initialState)
+new FirebaseServer(3000, '127.0.0.1', data)
