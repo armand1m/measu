551d0bd3b6b64019bad7979f738e3f8f0170def3
Armando Magalhães <armando.mag95@gmail.com>
2016-10-15 12:21:28 -0300

    feat(component/project-details): Project Details now is read only
    Make the project details panel read only.

--- app/actions/types.js
+++ app/actions/types.js
@@ -7,9 +7,6 @@ export const CHANGE_PROJECT = 'CHANGE_PROJECT'
 export const SET_PROJECTS = 'SET_PROJECTS'
 export const SET_CURRENT_PROJECT_KEY = 'SET_CURRENT_PROJECT_KEY'
 
-/** deprecated */
-export const SET_PROJECT = 'SET_PROJECT'
-
 export const SET_TASKS = 'SET_TASKS'
 export const ADD_TASK = 'ADD_TASK'
 export const CHANGE_TASK = 'CHANGE_TASK'
